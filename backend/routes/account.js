9e01e6e56eb8b4c0bcfe9e28e180ef74251861e6
cyybxrg Sourabh <cyborgop6@gmail.com>
2024-09-27 17:33:29 +0530

    fixed the bug of negative amount which could subtract money from others account

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require("../middlewares/user");
 const { Account } = require("../db/db");
 const { default: mongoose } = require("mongoose");
-
+const zod = require("zod")
 
 router.get("/balance", authMiddleware,async(req,res)=>{
     const exists = await Account.findOne({userId:req.userId});
@@ -24,55 +24,71 @@ router.get("/balance", authMiddleware,async(req,res)=>{
     }
 })
 
+const receiveSchema = zod.object({
+    to: zod.string(),
+    amount: zod.number().refine(n=>n>=1)
+})
 
 router.post("/transfer", authMiddleware, async(req,res)=>{
+
+    const parsePayload = receiveSchema.safeParse(req.body) 
     // starting a mongoose session
-    const session = await mongoose.startSession();
-    session.startTransaction();
 
-    // taking the inputs in body
-    const amount = parseFloat(req.body.amount);
-    const to = req.body.to;
+    if(parsePayload.success){
 
-    try{
-        // finding the "from account" does it exists or not
-        const fromAccount = await Account.findOne({userId: req.userId}).session(session);
-        if(!fromAccount || fromAccount.balance < amount){
-            session.abortTransaction();
-            return res.status(400).json({
-                msg: "insufficient Balance"
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        // taking the inputs in body
+        const amount = parseFloat(req.body.amount);
+        const to = req.body.to;
+
+        try{
+            // finding the "from account" does it exists or not
+            const fromAccount = await Account.findOne({userId: req.userId}).session(session);
+            if(!fromAccount || fromAccount.balance < amount){
+                session.abortTransaction();
+                return res.status(400).json({
+                    msg: "insufficient Balance"
+                })
+            }
+
+
+            // finding the "to account" does it exists or not
+            const toAccount = await Account.findOne({userId: to}).session(session)
+            if(!toAccount){
+                session.abortTransaction();
+                res.status(400).json({
+                    msg: "invalid account"
+                })
+            }
+
+            // processing the transaction for users
+            await Account.findOneAndUpdate({ userId: req.userId }, { $inc: { balance: -amount }}).session(session);
+            await Account.findOneAndUpdate({ userId: to }, {$inc:{balance: amount}}).session(session);
+
+            // committing the transaction
+            await session.commitTransaction();
+            res.status(200).json({
+                msg: "transfer successfull"
+            });
+        }catch(error){
+
+            res.status(500).json({
+                msg: "an error occured"
             })
+
+        }finally{
+
+            session.endSession();
         }
 
-
-        // finding the "to account" does it exists or not
-        const toAccount = await Account.findOne({userId: to}).session(session)
-        if(!toAccount){
-            session.abortTransaction();
-            res.status(400).json({
-                msg: "invalid account"
-            })
-        }
-
-        // processing the transaction for users
-        await Account.findOneAndUpdate({ userId: req.userId }, { $inc: { balance: -amount }}).session(session);
-        await Account.findOneAndUpdate({ userId: to }, {$inc:{balance: amount}}).session(session);
-
-        // committing the transaction
-        await session.commitTransaction();
-        res.status(200).json({
-            msg: "transfer successfull"
-        });
-    }catch(error){
-
+    }else{
         res.status(500).json({
-            msg: "an error occured"
+            msg: 'WRONG INPUT, enter a valid amount'
         })
-
-    }finally{
-
-        session.endSession();
     }
+    
         
 });
 
